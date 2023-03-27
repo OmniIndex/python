12ed6aee110e1ce865fd31ebd5bbf559da282083
github.com/jstanbri <jstanbridge@gmail.com>
2023-03-27 08:48:04 +0100

    Client and API Call

--- omniindex/api.py
+++ omniindex/api.py
@@ -1,39 +1,45 @@
 import requests
 import json
 
-# Constants
-_API_KEY = None
-_SERVER = None
+class OmniIndexClient:
+    """Instantiate an Omniindex client object to interact with the Omniindex API.
+    Parameters are:
+    :param server: The server to connect to. This can be either "testnet" or "mainnet".
+    :type server: str
+    :param api_key: The API key to use for authentication.
+    :type api_key: str
+    :param unit_name: The name of the unit to use for the transaction.
+    :type unit_name: str
+    :param user: The user to use for the transaction.
+    :type user: str
+    :param block_type: The type of block to use for the transaction.
+    """
+    def __init__(self, server, api_key, unit_name, block_type, user):
+        self.server = server
+        self.api_key = api_key
+        self.unit_name = unit_name
+        self.block_type = block_type
+        self.user = user
 
-# Functions
+    def get_block_schematic(self):
+        """Omniindex API call to get the schematic of a block.
 
-def api_key(api_key):
-    global _API_KEY
-    _API_KEY = api_key
+        Returns:
+            str: JSON string containing the block schematic.
+        """
+        url = "https://api.omniindex.xyz/api_v1/getblockschematic"
 
-def server(server_name):
-    global _SERVER
-    _SERVER = server_name
+        payload = json.dumps({
+            "unitName": self.unit_name,
+            "server": self.server,
+            "Type": self.block_type,
+            "user": self.user,
+            "password": self.api_key
+        })
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
 
-def unit_name(unit_name):
-    global _UNIT_NAME
-    _UNIT_NAME = unit_name
-
-def get_block_schematic(unitName, Type, user)):
-    url = "https://api.omniindex.xyz/api_v1/getblockschematic"
-
-    payload = json.dumps({
-        "unitName": _UNIT_NAME,
-        "server": _SERVER,
-        "Type": Type,
-        "user": user,
-        "password": _API_KEY
-    })
-    headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload)
-    return response.text
-
+        response = requests.request("POST", url, headers=headers, data=payload)
+        return response.text
