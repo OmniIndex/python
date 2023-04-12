55f20bb2717f0b5aadac1963975501c5c4f820ec
James Stanbridge <james@omniindex.io>
2023-04-12 18:30:44 +0000

    Update setup.py to v 0.1.11

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ HERE = path.abspath(path.dirname(__file__))
 
 setup(
     name="omniindex",
-    version="0.1.10",
+    version="0.1.11",
     packages=find_packages(),
     install_requires=[
         "requests",
