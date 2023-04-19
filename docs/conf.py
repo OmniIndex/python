5de8dbba6fc11cb4968c973a5f0fd49725f76835
James Stanbridge <james@purposebridge.uk>
2023-04-19 17:17:16 +0100

    💚 ci:(API): Removed TODO from documentation
    TODO is an extension in sphinx of the re-structured formatting that allows you to keep track of todo: items - now set to FALSE for publication purposes

--- docs/conf.py
+++ docs/conf.py
@@ -38,4 +38,4 @@ html_static_path = ['_static']
 html_domain_indices = True # https://stackoverflow.com/questions/56336234/build-fail-sphinx-error-contents-rst-not-found
 html_domain_indices = [('*','genindex', 'genindex.html', 4)]
 # Display todos by setting to True
-todo_include_todos = True
+todo_include_todos = False
